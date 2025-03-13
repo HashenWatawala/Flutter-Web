0c308f27fde1e1a0a335b539dff9f6fbf59722c5
HashenWatawala <hashanewatawala@gmail.com>
2025-03-13 22:17:32 +0530

    Sart to bulid Background and navbar

---
+++ lib/Navbar/navbar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Navbar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (context,constraints){
+          if(constraints.maxWidth>1200){
+            return DesktopNavbar();
+          } else if(constraints.maxWidth>800 && constraints.maxWidth<=1200){
+            return DesktopNavbar();
+          } else {
+            return MobileNavbar();
+          }
+        }
+    );
+  }
+}
+class DesktopNavbar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Text("Test WEB", style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30
+          ),),
+
+          Row(
+            children: <Widget>[
+             Text("Home", style: TextStyle(color: Colors.white),),
+
+              Text("Home", style: TextStyle(color: Colors.white),),
+              Text("Home", style: TextStyle(color: Colors.white),)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+class MobileNavbar extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:web_project/Navbar/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,16 +34,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class  extends StatelessWidget {
-  const ({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [Color.fromRGBO(66, 114, 245, 1), Color.fromRGBO(29, 25, 99, 1)]
+          )
+        ),
+        child:Column(
+          children: <Widget>[
+            Navbar()
+          ],
+        ),
+      ),
+    );
   }
 }
