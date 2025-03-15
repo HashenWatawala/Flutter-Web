2ba01f13f171c49a2e9717283d2f853c2fc1562d
HashenWatawala <hashanewatawala@gmail.com>
2025-03-15 22:50:59 +0530

    Mobile navbar created

--- lib/Navbar/navbar.dart
+++ lib/Navbar/navbar.dart
@@ -7,7 +7,7 @@ class Navbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (context,constraints){
-          if(constraints.maxWidth>1200){
+          if(constraints.maxWidth > 1200){
             return DesktopNavbar();
           } else if(constraints.maxWidth>800 && constraints.maxWidth<1200){
             return DesktopNavbar();
@@ -25,7 +25,7 @@ class DesktopNavbar extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
       child: Container(
-        constraints: BoxConstraints(maxWidth: 1200),
+        constraints: BoxConstraints(maxWidth: 4200),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
@@ -62,7 +62,29 @@ class MobileNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text("Test WEB", style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30
+          ),),
+
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Home", style: TextStyle(color: Colors.white),),
+                SizedBox(width: 30,),
+                Text("About", style: TextStyle(color: Colors.white),),
+                SizedBox(width: 30,),
+                Text("Portfolio", style: TextStyle(color: Colors.white),),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
